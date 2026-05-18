2ad1debf43cbd4dca09dfc6c170f76fc728b6b00
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-18 13:14:52 -0500

    commit - ApplicationDbContext .

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,36 @@
-﻿namespace BackendAPI.Data
+﻿using BackendAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendAPI.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+        public DbSet<Producto> Productos => Set<Producto>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>().ToTable("PRODUCTOS");
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Id)
+                .HasColumnName("ID");
+
+                entity.Property(e => e.Nombre)
+                .HasColumnName("NOMBRE");
+
+                entity.Property(e => e.Precio)
+                .HasColumnName("PRECIO");
+
+                entity.Property(e => e.Stock)
+                .HasColumnName("STOCK");
+            });
+        }
     }
-}
+}   
